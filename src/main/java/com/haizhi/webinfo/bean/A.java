41172d7999899564172bf9a965dad634449e4239
zhangjunfei <zhangjunfei@haizhi.com>
2017-09-02 16:59:58 +0800

    config log4j2

---
+++ src/main/java/com/haizhi/webinfo/bean/A.java
@@ -0,0 +1,17 @@
+package com.haizhi.webinfo.bean;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by haizhi on 2017/9/2.
+ */
+@Component
+public class A {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("A init");
+    }
+}
--- src/main/java/com/haizhi/webinfo/controller/MyController.java
+++ src/main/java/com/haizhi/webinfo/controller/MyController.java
@@ -1,5 +1,7 @@
 package com.haizhi.webinfo.controller;
 
+import com.haizhi.webinfo.bean.A;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MyController {
+
+    @Autowired
+    A a;
 
     @RequestMapping("/getTest")
     public String test(){

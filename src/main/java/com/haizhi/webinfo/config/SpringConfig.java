e94a8f1c97bcae1b4e725bc31afe5e623f648741
18575531337 <511980821@qq.com>
2017-09-05 22:46:16 +0800

    增加spring权限模块

--- src/main/java/com/haizhi/webinfo/config/SpringConfig.java
+++ src/main/java/com/haizhi/webinfo/config/SpringConfig.java
@@ -1,13 +1,21 @@
 package com.haizhi.webinfo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by haizhi on 2017/9/2.
  */
 @ComponentScan(basePackages = "com.haizhi.webinfo")
 @Configuration
-public class SpringConfig {
+@Import(SpringConfigSecurity.class)
+public class SpringConfig  {
 
 }

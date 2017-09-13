c265fa1aad02d371919641f786def7c73346de61
18575531337 <511980821@qq.com>
2017-09-13 23:35:52 +0800

    增加spring权限模块

---
+++ src/main/java/com/haizhi/webinfo/constants/Role.java
@@ -0,0 +1,11 @@
+package com.haizhi.webinfo.constants;
+
+/**
+ * Created by JuniFire on 2017/9/13.
+ */
+public interface Role {
+
+    String USER = "USER";
+    String ADMIN = "ADMIN";
+    String SUPER_ADMIN = "super_admin";
+}
